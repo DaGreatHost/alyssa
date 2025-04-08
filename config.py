66c1c7770ee739881fb107b8c3cb847cd7fe0fe4
DaGreatHost <heherson230@gmail.com>
2025-04-09 02:08:22 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-PAYMENT_LINKS = {
-    "GCASH_MAYA": "https://t.me/Scan2payv1bot?startapp=pay",
-    "TON": "https://app.tonkeeper.com/transfer/UQAwroBrBTSzzVYx_IXpR-R_KJ_mZQgmT7uNsUZdJ5MM68ep?amount=2099700000&text=exclusivechannel"
-}
